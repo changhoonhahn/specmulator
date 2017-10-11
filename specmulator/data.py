f8ba35bb8440460d2f7ebf2cf3d3920858a29ff8
ChangHoon Hahn <chh327@nyu.edu>
2017-10-11 00:17:04 -0700

    setting things up

---
+++ specmulator/data.py
@@ -0,0 +1,43 @@
+'''
+
+
+'''
+import bigfile as BF 
+from nbodykit.lab import HODCatalog
+#from nbodykit.lab import HaloCatalog
+
+
+def Galaxies(halos, p_hod, seed=None): 
+    ''' populate given halo catalog (halos) with galaxies
+    based on HOD model with p_hod parameters 
+
+    Parameters
+    ----------
+    halos : halotools.sim_manager.UserSuppliedHaloCatalog
+        output of HaloCatalog.to_halotools. for our intents and purposes 
+        this is the halo catalog
+    p_hod : dict
+        dictionary specifying the HOD parameters 
+    '''
+    if 'alpha' not in p_hod.keys(): 
+        raise ValueError
+    if 'logMmin' not in p_hod.keys(): 
+        raise ValueError
+    if 'logM1' not in p_hod.keys(): 
+        raise ValueError
+    if 'logM0' not in p_hod.keys(): 
+        raise ValueError
+    if 'sigma_logM' not in p_hod.keys(): 
+        raise ValueError
+
+    # run HOD
+    halocat = halos.to_halotools(halos.attrs['BoxSize'])
+    hod = HODCatalog(halocat, seed=seed, **params)
+    
+    hod.save('%s/HOD'%(folder), ('Position', 'Velocity'))
+   
+
+
+def Halos(): 
+    '''
+    '''
