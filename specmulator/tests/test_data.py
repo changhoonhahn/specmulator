0eda8d5904d4da4c9cdb3db3241dd86ea1994714
ChangHoon Hahn <chh327@nyu.edu>
2017-10-25 19:52:54 -0700

    preparing to start calculations on NERSC

--- specmulator/tests/test_data.py
+++ specmulator/tests/test_data.py
@@ -7,10 +7,10 @@ test data.py
 import env 
 import data as Data 
 
-def runNeutParticles():
+def runNeutParticles(nreal):
     ''' Test that the method NeutrinoHalos works properly 
     '''
-    cat = Data.NeutParticles(0.0, 1, 4, clobber=True)  # mneut = 0.0eV, realization #1, z = 0 
+    cat = Data.NeutParticles(0.0, nreal, 4, clobber=True)  # mneut = 0.0eV, realization #1, z = 0 
     print type(cat)
     return None 
 
@@ -24,5 +24,5 @@ def runNeutHalos():
 
 
 if __name__=="__main__": 
-    #runNeutParticles()
-    runNeutHalos()
+    for i in range(2,11): 
+        runNeutParticles(i)
