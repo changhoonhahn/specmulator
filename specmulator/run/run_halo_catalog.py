2de45d6b53b8ebbe2d249fb7fd151c495e12f6b4
chang.nersc <changh20@gmail.com>
2017-11-06 13:32:21 -0800

    3pcf and plk evaluations for the halo catalogs

--- specmulator/run/run_halo_catalog.py
+++ specmulator/run/run_halo_catalog.py
@@ -15,16 +15,32 @@ import data as Dat
 import forwardmodel as FM 
 
 
-def NeutHalo_Plk(mneut, nreal, nzbin): 
+def NeutHalo_Plk(mneut, nreal, nzbin, zspace=False): 
     ''' Calculate the powerspectrum multipoles for Paco's Neutrio 
     halo catalogs
     '''
     # import Neutrino halo with mneut eV, realization # nreal, at z specified by nzbin 
     halos = Dat.NeutHalos(mneut, nreal, nzbin) 
+    if zspace: 
+        halos['RSDPosition'] = FM.RSD(halos, LOS=[0,0,1])
+    
+    # calculate P_l(k) 
+    plk = FM.Observables(halos, observable='plk', rsd=zspace, Nmesh=360)
 
-    plk = FM.Observables(halos, observable='plk', rsd=False, Nmesh=360)
+    # output file 
+    if mneut == 0.1: dir = ''.join([UT.dat_dir(), '0.10eV/'])
+    else: dir = ''.join([UT.dat_dir(), str(mneut), 'eV/'])
+    if zspace: str_space = '.z'
+    else: str_space = '.r'
+    fout = ''.join([dir, 'plk.groups.', str(mneut), 'eV.', str(nreal), '.nzbin', str(nzbin), str_space, 'space.dat']) 
 
-    # save somehow 
+    # header 
+    hdr = ''.join(['P_l(k) measurements for m neutrino = ', str(mneut), ' eV, realization ', str(nreal), ', zbin ', str(nzbin), 
+        '\n P_shotnoise ', str(plk['shotnoise']), 
+        '\n cols: k, P_0, P_2, P_4']) 
+
+    # write to file 
+    np.savetxt(fout, np.array([plk['k'], plk['p0k'], plk['p2k'], plk['p4k']]).T, header=hdr) 
     return None 
 
 
@@ -55,32 +71,34 @@ def NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=False):
     else: dir = ''.join([UT.dat_dir(), str(mneut), 'eV/', str(nreal)])
     if zspace: str_space = '.z'
     else: str_space = '.r'
-    fout = ''.join([dir, '/groups_', str(nzbin).zfill(3), '/groups', str_space, 'space.dat']) 
+    fout = ''.join([dir, '/groups.nzbin', str(nzbin), '.', str_space, 'space.dat']) 
     
     # header 
     hdr = ''.join(['m neutrino = ', str(mneut), ' eV, realization ', str(nreal), ', zbin ', str(nzbin)]) 
 
     # write to file 
     np.savetxt(fout, np.array([x, y, z, w]).T, header=hdr) 
+    print('--- halo written to ---\n %s' % (fout))
     return None 
 
 
 if __name__=="__main__": 
-    arg1 = Sys.argv[1]    
+    arg1 = Sys.argv[1] # 3pcf/plk etc 
 
-    if arg1 == '3pcf': 
-        mneut = float(Sys.argv[2])
-        nreal = int(Sys.argv[3])
-        nzbin = int(Sys.argv[4])
-        zstr = Sys.argv[5]
-        if zstr == 'z': 
-            zbool = True
-        elif zstr == 'real': 
-            zbool = False
-        else: 
-            raise ValueError
-
-        NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=zbool)
+    mneut = float(Sys.argv[2])
+    nreal = int(Sys.argv[3])
+    nzbin = int(Sys.argv[4])
+    zstr = Sys.argv[5]
+    if zstr == 'z': 
+        zbool = True
+    elif zstr == 'real': 
+        zbool = False
     else: 
         raise ValueError
 
+    if arg1 == '3pcf': 
+        NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=zbool)
+    elif arg1 == 'plk': 
+        NeutHalo_Plk(mneut, nreal, nzbin, zspace=zbool)
+    else: 
+        raise ValueError
