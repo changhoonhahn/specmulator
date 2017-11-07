43e1d0406e42e9c7ba7b26fb87a005c24d8855ec
chang.nersc <changh20@gmail.com>
2017-11-06 23:16:01 -0800

    running 3pcf preprocessing on edison

--- specmulator/run/run_halo_catalog.py
+++ specmulator/run/run_halo_catalog.py
@@ -8,6 +8,7 @@ import os
 import time 
 import sys as Sys
 import numpy as np 
+from interruptible_pool import InterruptiblePool as Pewl
 
 import env 
 import lhd
@@ -45,7 +46,7 @@ def NeutHalo_Plk(mneut, nreal, nzbin, zspace=False):
     return None 
 
 
-def NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=False): 
+def NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=False, clobber=False): 
     ''' Pre-process halo catalogs for 3PCF run. Read in halo catalog
     and output to input file format for Daniel Eisenstein's code. 
 
@@ -55,6 +56,17 @@ def NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=False):
         if True, calculates redshift space positions. Otherwise real space. 
 
     '''
+    # output file 
+    if mneut == 0.1: dir = ''.join([UT.dat_dir(), '0.10eV/', str(nreal)])
+    else: dir = ''.join([UT.dat_dir(), str(mneut), 'eV/', str(nreal)])
+    if zspace: str_space = '.z'
+    else: str_space = '.r'
+    fout = ''.join([dir, '/groups.nzbin', str(nzbin), str_space, 'space.dat']) 
+
+    if os.path.isfile(fout) and not clobber: 
+        print('--- already written to ---\n %s' % (fout))
+        return None 
+
     # import Neutrino halo with mneut eV, realization # nreal, at z specified by nzbin 
     halos = Dat.NeutHalos(mneut, nreal, nzbin) 
     if zspace: 
@@ -68,13 +80,6 @@ def NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=False):
     # weights (all ones!) 
     w = np.ones(len(x)) 
     
-    # output file 
-    if mneut == 0.1: dir = ''.join([UT.dat_dir(), '0.10eV/', str(nreal)])
-    else: dir = ''.join([UT.dat_dir(), str(mneut), 'eV/', str(nreal)])
-    if zspace: str_space = '.z'
-    else: str_space = '.r'
-    fout = ''.join([dir, '/groups.nzbin', str(nzbin), '.', str_space, 'space.dat']) 
-    
     # header 
     hdr = ''.join(['m neutrino = ', str(mneut), ' eV, realization ', str(nreal), ', zbin ', str(nzbin)]) 
 
@@ -85,23 +90,51 @@ def NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=False):
     return None 
 
 
+def _NeutHalo_pre3PCF(args):
+    # wrapper for NeutHalo that works with multiprocessing 
+    mneut = args[0]
+    nreal = args[1] 
+    nzbin = args[2] 
+    zspace = args[3]
+    NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=zbool)
+    return None
+
+
 if __name__=="__main__": 
     arg1 = Sys.argv[1] # 3pcf/plk etc 
-
     mneut = float(Sys.argv[2])
-    nreal = int(Sys.argv[3])
-    nzbin = int(Sys.argv[4])
-    zstr = Sys.argv[5]
-    if zstr == 'z': 
-        zbool = True
-    elif zstr == 'real': 
-        zbool = False
-    else: 
-        raise ValueError
+
 
     if arg1 == '3pcf': 
-        NeutHalo_pre3PCF(mneut, nreal, nzbin, zspace=zbool)
+        nreal_i = int(Sys.argv[3])
+        nreal_f = int(Sys.argv[4])
+        nzbin = int(Sys.argv[5])
+        zstr = Sys.argv[6]
+        if zstr == 'z': 
+            zbool = True
+        elif zstr == 'real': 
+            zbool = False
+        else: 
+            raise ValueError
+        nthreads = int(Sys.argv[7])
+        
+        args_list = [(mneut, ireal, nzbin, zbool) for ireal in np.arange(nreal_i, nreal_f+1)]
+        pool = Pewl(processes=nthreads)
+        mapfn = pool.map
+        results = mapfn(_NeutHalo_pre3PCF, args_list) 
+        pool.close()
+        pool.terminate()
+        pool.join()
     elif arg1 == 'plk': 
+        nreal = int(Sys.argv[3])
+        nzbin = int(Sys.argv[4])
+        zstr = Sys.argv[5]
+        if zstr == 'z': 
+            zbool = True
+        elif zstr == 'real': 
+            zbool = False
+        else: 
+            raise ValueError
         NeutHalo_Plk(mneut, nreal, nzbin, zspace=zbool)
     else: 
         raise ValueError
