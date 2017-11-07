728f4e83cd11a24d486152f8e8852a4baf08aeeb
ChangHoon Hahn <chh327@nyu.edu>
2017-11-07 10:10:26 -0800

    observables implemented

---
+++ specmulator/obvs.py
@@ -0,0 +1,46 @@
+'''
+
+code for dealing with observables
+
+'''
+import os 
+import numpy as np 
+
+import util as UT 
+
+
+def Plk_halo(mneut, nreal, nzbin, zspace=False): 
+    ''' Return the powerspectrum multipoles for halo catalog with
+    total neutrino mass `mneut`, realization `nreal`, and redshift 
+    bin `nzbin` in either real/redshift space. 
+
+    '''
+    dir = ''.join([UT.dat_dir(), str(mneut), 'eV/' ]) 
+    if zspace: str_space = 'z' # redhsift
+    else: str_space = 'r' # real 
+    f = ''.join([dir, 'plk.groups.', str(mneut), 'eV.', str(nreal), '.nzbin', str(nzbin), 
+        '.', str_space, 'space.dat']) 
+    if not os.path.isfile(f): raise ValueError('file does not exist') 
+
+    # read in plk 
+    k,p0k,p2k,p4k = np.loadtxt(f, skiprows=3, unpack=True, usecols=[0,1,2,3]) 
+    plk = {} 
+    plk['k'] = k 
+    plk['p0k'] = p0k 
+    plk['p2k'] = p2k 
+    plk['p4k'] = p4k 
+
+    # readin shot-noise from header 
+    with open(f) as lines: 
+        for i_l, line in enumerate(lines):
+            if i_l == 1: 
+                str_sn = line
+                break 
+    plk['shotnoise'] = float(str_sn.strip().split('P_shotnoise')[-1])
+    return plk 
+
+
+def threePCF_halo(mneut, nreal, nzbin, zspace=False): 
+    ''' Read in three point data. Need to figure out how I want to do this...
+    '''
+    pass 
---
+++ specmulator/tests/test_obvs.py
@@ -0,0 +1,59 @@
+import numpy as np 
+
+import env
+import util as UT
+import obvs as Obvs
+
+import matplotlib as mpl 
+import matplotlib.pyplot as plt
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['axes.linewidth'] = 1.5
+mpl.rcParams['axes.xmargin'] = 1
+mpl.rcParams['xtick.labelsize'] = 'x-large'
+mpl.rcParams['xtick.major.size'] = 5
+mpl.rcParams['xtick.major.width'] = 1.5
+mpl.rcParams['ytick.labelsize'] = 'x-large'
+mpl.rcParams['ytick.major.size'] = 5
+mpl.rcParams['ytick.major.width'] = 1.5
+mpl.rcParams['legend.frameon'] = False
+
+
+def Plk_halo(mneut=0.0, nzbin=4, zspace=False): 
+    ''' Test the Plk_halo 
+    '''
+    p0ks, p2ks, p4ks = [], [], [] 
+    for ireal in range(1, 51):
+        plk_i = Obvs.Plk_halo(mneut, ireal, nzbin, zspace=zspace) 
+        if ireal == 1: k = plk_i['k']
+        p0ks.append(plk_i['p0k']) 
+        p2ks.append(plk_i['p2k']) 
+        p4ks.append(plk_i['p4k']) 
+    
+    fig = plt.figure() 
+    sub = fig.add_subplot(111)
+    for p0k, p2k, p4k in zip(p0ks, p2ks, p4ks): 
+        sub.plot(k, k * p0k, c='k', lw=0.1) 
+        sub.plot(k, k * p2k, c='b', lw=0.1) 
+        sub.plot(k, k * p4k, c='r', lw=0.1) 
+    # plot the average 
+    sub.plot(k, k * np.average(np.array(p0ks), axis=0), c='k', lw=2, ls='--', label='$\ell=0$') 
+    sub.plot(k, k * np.average(np.array(p2ks), axis=0), c='b', lw=2, ls='--', label='$\ell=2$') 
+    sub.plot(k, k * np.average(np.array(p4ks), axis=0), c='r', lw=2, ls='--', label='$\ell=4$') 
+
+    sub.set_xlim([0.01, 0.15]) 
+    sub.set_xlabel('k', fontsize=20) 
+
+    sub.set_ylim([-2000., 2500.]) 
+    sub.set_ylabel('$k P(k)$', fontsize=20) 
+    sub.legend(loc='lower right', prop={'size': 15}) 
+    if zspace: str_space = 'z' 
+    else: str_space = 'r'
+    fig.savefig(''.join([UT.fig_dir(), 'tests/plk_halo.', str(mneut), 'eV.nzbin', str(nzbin), 
+        '.', str_space, 'space.png']), bbox_inches='tight') 
+    return None 
+
+
+if __name__=="__main__": 
+    Plk_halo(zspace=False)
+    Plk_halo(zspace=True)
